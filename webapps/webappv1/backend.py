78a85cfac39f224acc1a9ea1d631afdb82fb51d5
MichelPT <michelpt76767@gmail.com>
2025-07-21 20:54:37 +0700

    normli dd

--- webapps/webappv1/backend.py
+++ webapps/webappv1/backend.py
@@ -1400,4 +1400,4 @@ def get_normalization_plot():
         return {"status": "error", "message": str(e)}
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
